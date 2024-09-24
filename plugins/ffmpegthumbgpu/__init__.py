d5e3366146a41ee765d2d05383cb5a5570546561
NevermoreN <dota_sf@msn.cn>
2024-09-24 18:31:22 +0800

    Update __init__.py

--- plugins/ffmpegthumbgpu/__init__.py
+++ plugins/ffmpegthumbgpu/__init__.py
@@ -28,7 +28,7 @@ class FFmpegThumb(_PluginBase):
     # 插件图标
     plugin_icon = "ffmpeg.png"
     # 插件版本
-    plugin_version = "1.3"
+    plugin_version = "1.4"
     # 插件作者
     plugin_author = "NevermoreN"
     # 作者主页
