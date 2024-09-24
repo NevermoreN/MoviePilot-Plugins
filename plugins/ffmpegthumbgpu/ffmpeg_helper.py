b0e706ee2fa4a1fd450dc4828e7ca001467646ad
NevermoreN <dota_sf@msn.cn>
2024-09-24 19:05:42 +0800

    Update ffmpeg_helper.py

--- plugins/ffmpegthumbgpu/ffmpeg_helper.py
+++ plugins/ffmpegthumbgpu/ffmpeg_helper.py
@@ -1,5 +1,6 @@
 import json
 import subprocess
+
 from app.utils.system import SystemUtils
 
 
@@ -8,17 +9,15 @@ class FfmpegHelper:
     @staticmethod
     def get_thumb(video_path: str, image_path: str, frames: str = None):
         """
-        使用ffmpeg从视频文件中截取缩略图，利用GPU加速
+        使用ffmpeg从视频文件中截取缩略图
         """
         if not frames:
             frames = "00:03:01"
         if not video_path or not image_path:
             return False
-        cmd = 'ffmpeg -hwaccel cuda -i "{video_path}" -ss {frames} -vframes 1 -f image2 "{image_path}"'.format(
-            video_path=video_path,
-            frames=frames,
-            image_path=image_path
-        )
+        cmd = 'ffmpeg -hwaccel cuda -i "{video_path}" -ss {frames} -vframes 1 -f image2 "{image_path}"'.format(video_path=video_path,
+                                                                                                              frames=frames,
+                                                                                                              image_path=image_path)
         result = SystemUtils.execute(cmd)
         if result:
             return True
@@ -27,18 +26,18 @@ class FfmpegHelper:
     @staticmethod
     def extract_wav(video_path: str, audio_path: str, audio_index: str = None):
         """
-        使用ffmpeg从视频文件中提取16000hz, 16-bit的wav格式音频，利用GPU加速
+        使用ffmpeg从视频文件中提取16000hz, 16-bit的wav格式音频
         """
         if not video_path or not audio_path:
             return False
 
         # 提取指定音频流
         if audio_index:
-            command = ['ffmpeg', "-hide_banner", "-loglevel", "warning", '-hwaccel', 'cuda', '-y', '-i', video_path,
+            command = ['ffmpeg', '-hwaccel', 'cuda', "-hide_banner", "-loglevel", "warning", '-y', '-i', video_path,
                        '-map', f'0:a:{audio_index}',
                        '-acodec', 'pcm_s16le', '-ac', '1', '-ar', '16000', audio_path]
         else:
-            command = ['ffmpeg', "-hide_banner", "-loglevel", "warning", '-hwaccel', 'cuda', '-y', '-i', video_path,
+            command = ['ffmpeg', '-hwaccel', 'cuda', "-hide_banner", "-loglevel", "warning", '-y', '-i', video_path,
                        '-acodec', 'pcm_s16le', '-ac', '1', '-ar', '16000', audio_path]
 
         ret = subprocess.run(command).returncode
@@ -66,16 +65,17 @@ class FfmpegHelper:
     @staticmethod
     def extract_subtitle(video_path: str, subtitle_path: str, subtitle_index: str = None):
         """
-        从视频中提取字幕，利用GPU加速
+        从视频中提取字幕
         """
         if not video_path or not subtitle_path:
             return False
 
         if subtitle_index:
-            command = ['ffmpeg', "-hide_banner", "-loglevel", "warning", '-hwaccel', 'cuda', '-y', '-i', video_path,
-                       '-map', f'0:s:{subtitle_index}', subtitle_path]
+            command = ['ffmpeg', '-hwaccel', 'cuda', "-hide_banner", "-loglevel", "warning", '-y', '-i', video_path,
+                       '-map', f'0:s:{subtitle_index}',
+                       subtitle_path]
         else:
-            command = ['ffmpeg', "-hide_banner", "-loglevel", "warning", '-hwaccel', 'cuda', '-y', '-i', video_path, subtitle_path]
+            command = ['ffmpeg', '-hwaccel', 'cuda', "-hide_banner", "-loglevel", "warning", '-y', '-i', video_path, subtitle_path]
         ret = subprocess.run(command).returncode
         if ret == 0:
             return True
